515d093542e70cbbc07ced893ab3809581787cf4
3EyEdRav3n <Louie.Syphr@gmail.com>
2019-10-15 22:37:35 -0400

    updating Arduino

--- MyFirstProgram/MyFirstProgram.ino
+++ MyFirstProgram/MyFirstProgram.ino
@@ -1,9 +1,29 @@
+int button_press = 1;
+
 void setup() {
   // put your setup code here, to run once:
-
-}
+  int first_num = 5;
+  int second_num = 9;
+  
+  Serial.begin(9600);
+  
+  switch(button_press){
+    case 1:
+      Serial.println("Button 1 pressed");
+      break;
+    case 2:
+      Serial.println("Button 2 pressed");
+      break;
+    default:
+      Serial.println("I don't know what was pressed.");
+      break;
+  }
+} // end setup()
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+} // end loop()
 
-}
+int do_a_calc(int num1, int num2){
+  return (num1 + num2);
+} 
